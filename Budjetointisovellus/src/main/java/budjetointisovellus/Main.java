5d4fb2210c576a1999a5e8a6ee859143d4490238
Anni Ala-Mattinen <anni.alamattinen@gmail.com>
2021-03-25 11:41:26 +0200

    luotu budjetUi

--- Budjetointisovellus/src/main/java/budjetointisovellus/Main.java
+++ Budjetointisovellus/src/main/java/budjetointisovellus/Main.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package budjetointisovellus;
 
-/**
- *
- * @author alaanni
- */
+import budjetointisovellus.ui.BudjetUi;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        BudjetUi.main(args);
+    }
 }
---
+++ Budjetointisovellus/src/main/java/budjetointisovellus/ui/BudjetUi.java
@@ -0,0 +1,19 @@
+package budjetointisovellus.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class BudjetUi extends Application {
+    
+    @Override
+    public void start(Stage ikkuna) {
+        ikkuna.setTitle("Hei Maailma!");
+        ikkuna.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
