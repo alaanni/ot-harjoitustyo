0e898e30afae2250c4dab05d44724d24756ffe10
Anni Ala-Mattinen <anni.alamattinen@gmail.com>
2021-03-26 15:59:49 +0200

    luotu luokkia

--- Budjetointisovellus/src/main/java/budjetointisovellus/domain/ActualCost.java
+++ Budjetointisovellus/src/main/java/budjetointisovellus/domain/ActualCost.java
@@ -1,11 +1,18 @@
 package budjetointisovellus.domain;
 
+import java.util.*;
+
 /**
- *
  * Toteutunutta kulua edustava luokka
  */
+
 public class ActualCost extends BudgetedCost {
     
-  
+    private Date date;
+    
+    public ActualCost(Date date, String name, int cost, Category category) {
+        super(name, cost, category);
+        this.date = date;
+    }
     
 }
--- Budjetointisovellus/src/main/java/budjetointisovellus/domain/BudgetedCost.java
+++ Budjetointisovellus/src/main/java/budjetointisovellus/domain/BudgetedCost.java
@@ -1,12 +1,21 @@
 package budjetointisovellus.domain;
 
+import java.util.*;
+
 /**
  *
  * Suunniteltua kulua edustava luokka
  */
 
 public class BudgetedCost {
+    
     private String name;
     private int cost;
-    private String kategory;
+    private Category category;
+    
+    public BudgetedCost(String name, int cost, Category category) {
+        this.name = name;
+        this.cost = cost;
+        this.category = category;
+    }
 }
